90cbcba70acd6b249de44a6386d9a36cbbd10779
Ash <ashley.tulett@hotmail.com>
2021-01-18 22:15:59 +1100

    Added questions.spec.js

---
+++ src/reducers/questions.spec.js
@@ -0,0 +1,28 @@
+import { questions } from "./questions";
+
+describe("The questions reducer", () => {
+  it("should not modify state for unrecognised action", () => {
+    console.log("Testing questions");
+    const state = ["foo", "bar"];
+    const stateClone = ["foo", "bar"];
+    const newState = questions(state, { type: "UNRECOGNISED ACTION" });
+    expect(newState).toEqual(stateClone);
+    expect(newState).toBe(state);
+  });
+
+  it("should add new questions", () => {
+    const state = [{ question_id: "foo" }, { question_id: "bar" }];
+    const newQuestion = { question_id: "baz" };
+    const newQuestionClone = { question_id: "baz" };
+    const newState = questions(state, {
+      type: "FETCHED_QUESTION",
+      question: newQuestion,
+    });
+
+    console.log(newState);
+    expect(newState).toContain(newQuestion);
+    // expect(newState).toContain(newQuestionClone); // toContain works like toBe
+    expect(state).not.toContain(newQuestion);
+    expect(newState).toHaveLength(3);
+  });
+});
